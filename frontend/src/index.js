2c2ec5f75f4006c434cb650202c99e12c7807d11
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-02-02 22:43:29 +0400

    css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import QueryProducts from './pages/QueryProducts';
+import './index.css';
 const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
